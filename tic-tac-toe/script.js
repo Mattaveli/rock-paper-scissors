7ed473031e501f5b67159e237893a1b309062a2f
Mattaveli <matthew0495@outlook.com>
2022-07-06 19:46:56 -0400

    Version 0.1 with UI changes

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -1,20 +1,21 @@
 let computerPlay = () => {
   let playChoiceArr = ["rock", "paper", "scissors"];
   let choice = Math.floor(Math.random() * playChoiceArr.length);
+  console.log("CPU: " + playChoiceArr[choice]);
   return playChoiceArr[choice];
 };
 
 let playerPlay = () => {
-  let playChoiceArr = ["rock", "paper", "scissors"];
-  let playerChoice = prompt("Choose rock, paper, or scissors.").toLowerCase();
-
-  while (playChoiceArr.indexOf(playerChoice) == -1) {
-    playerChoice = prompt(
-      "Invalid choice. Please choose rock, paper, or scissors."
-    );
-  }
-
-  return playerChoice;
+  const buttons = document.querySelectorAll("#btn");
+  let choice;
+  buttons.forEach((button) => {
+    // and for each one we add a 'click' listener
+    button.addEventListener("click", () => {
+      choice = button.value;
+      console.log(choice);
+      return choice;
+    });
+  });
 };
 
 let getWinner = (playerChoice, computerChoice) => {
@@ -38,30 +39,109 @@ let getWinner = (playerChoice, computerChoice) => {
   return result;
 };
 
-let game = () => {
-  let playerScore = 0;
-  let cpuScore = 0;
-  for (let i = 0; i < 5; i++) {
-    let player = playerPlay();
-    let result = getWinner(player, computerPlay());
-    console.log(result);
-    if (result.startsWith("You Win")) {
-      playerScore += 1;
-    } else if (result.startsWith("You Lose.")) {
-      cpuScore += 1;
-    }
+let updateResultsSection = (newMessage) => {
+  message.textContent = newMessage;
+  playerScoreCounter.textContent = "You: " + playerScore;
+  cpuScoreCounter.textContent = "CPU: " + cpuScore;
+};
+
+let setIcons = (playerChoice, cpuChoice) => {
+  switch (cpuChoice) {
+    case "rock":
+      cpuIcon.textContent = "🪨";
+      break;
+    case "paper":
+      cpuIcon.textContent = "🧻";
+      break;
+    case "scissors":
+      cpuIcon.textContent = "✂️";
+      break;
   }
-  console.log("Player:" + playerScore);
-  console.log("CPU Score: " + cpuScore);
-  if (playerScore == cpuScore) {
-    alert("Tie Game!");
-  } else {
-    playerScore > cpuScore ? alert("You Win! 👍") : alert("You Lose 👎");
+
+  switch (playerChoice) {
+    case "rock":
+      playerIcon.textContent = "🪨";
+      break;
+    case "paper":
+      playerIcon.textContent = "🧻";
+      break;
+    case "scissors":
+      playerIcon.textContent = "✂️";
+      break;
   }
 };
+
+let game = (playerSelection) => {
+  let cpu = computerPlay();
+  setIcons(playerSelection, cpu);
+  let winner = getWinner(playerSelection, cpu);
+  console.log(winner);
+  if (winner.startsWith("You Win")) {
+    playerScore += 1;
+    updateResultsSection("Winner!");
+    console.log("Your score: " + playerScore + " CPU Score: " + cpuScore);
+  } else if (winner.startsWith("You Lose.")) {
+    cpuScore += 1;
+    updateResultsSection("Loser :(");
+    console.log("Your score: " + playerScore + " CPU Score: " + cpuScore);
+  } else if (winner.startsWith("Tie")) {
+    updateResultsSection("Tie Game");
+  }
+  let gameOver = isGameOVer(cpuScore, playerScore);
+  if (gameOver) {
+    resetGame();
+  }
+};
+let isGameOVer = (cpuScore, playerScore) => {
+  let gameOver = false;
+  if (playerScore == numOfGames) {
+    alert("You Win! 👍");
+    gameOver = true;
+  }
+  if (cpuScore == numOfGames) {
+    alert("You Lose 👎");
+    gameOver = true;
+  }
+
+  return gameOver;
+};
+
+let resetGame = () => {
+  playerScore = 0;
+  cpuScore = 0;
+  updateResultsSection(" ");
+  numOfGames = getNumOfGames();
+};
+
+const buttons = document.querySelectorAll("#btn");
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    choice = button.value;
+    game(choice);
+  });
+});
+
+let getNumOfGames = () => {
+  let userInput = prompt("What score would you like to play up to?").trim();
+  while (!userInput) {
+    userInput = prompt("What score would you like to play up to?").trim();
+  }
+
+  return userInput;
+};
+
+let playerScore = 0;
+let cpuScore = 0;
+let numOfGames = getNumOfGames();
+let playerScoreCounter = document.querySelector(".player-container");
+let cpuScoreCounter = document.querySelector(".cpu-container");
+let message = document.querySelector(".message");
+let playerIcon = document.querySelector(".player-icon");
+let cpuIcon = document.querySelector(".cpu-icon");
+
 /*let cpu = computerPlay();
   console.log("cpu: " + cpu);
   let msg = getWinner(player, cpu);
   alert(msg);
   console.log(game());*/
-btn.addEventListener("click", game);
+//window.addEventListener("load", game);
